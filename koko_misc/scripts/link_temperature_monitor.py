1081054521a3ced956e596056ab9a91089de22d1
Brent Yi <yibrenth@gmail.com>
2018-04-05 19:11:12 -0500

    added temperature printing script

---
+++ koko_misc/scripts/link_temperature_monitor.py
@@ -0,0 +1,48 @@
+import rospy
+from geometry_msgs.msg import Vector3
+from koko_hardware_drivers.msg import MotorState
+import numpy as np
+import tf.transformations as transformations
+
+REPORT_FREQ = 2
+
+temperatures = {}
+
+def get_temp(msg):
+    temp = {} # pun intended
+    for i, name in enumerate(msg.name):
+        temp[name] = msg.temperature[i]
+    temperatures = temp
+
+#################################################################################################
+
+def main():
+    global x_accum
+    global y_accum
+    global z_accum
+    global num
+
+    rospy.init_node('temperature_monitor', anonymous=True)
+    rospy.Subscriber("koko_hardware/motor_states", MotorState, get_temp, queue_size=1)
+
+    first = True
+    r = rospy.Rate(REPORT_FREQ)
+    motor_names = []
+    while not rospy.is_shutdown():
+        if len(temperatures) > 0:
+            values = []
+            if first:
+                motor_names = sorted(temperatures.keys())
+                values.append("time")
+                values.extend(motor_names)
+            else:
+                values.append(rospy.get_time())
+                for name in motor_names:
+                    values.append(temperatures[name])
+
+            print(','.join(values))
+            first = False
+        r.sleep()
+
+if __name__ == '__main__':
+    main()
