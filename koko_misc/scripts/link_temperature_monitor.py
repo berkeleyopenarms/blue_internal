1e0b20a6140e4bd596f799ecf58df8ddd9f8c336
Brent Yi <yibrenth@gmail.com>
2018-04-13 16:48:42 -0500

    temperature montering

--- koko_misc/scripts/link_temperature_monitor.py
+++ koko_misc/scripts/link_temperature_monitor.py
@@ -6,17 +6,39 @@ from koko_hardware_drivers.msg import MotorState
 import numpy as np
 import tf.transformations as transformations
 
+#
+# The output of this script is intended to be piped to a CSV file
+# $ rosrun koko_misc link_temperature_monitor.py | tee ~/temp_log.csv
+#
+
 REPORT_FREQ = 2
 
 temperatures = {}
+d_currents = {}
+q_currents = {}
 
 def get_temp(msg):
-    global temperatures
+    # in retrospect, we could probably just save
+    # the msg object instead of using all these
+    # global dicts
 
+    global temperatures
     temp = dict(temperatures) # pun intended
     for i, name in enumerate(msg.name):
         temp[name] = msg.temperature[i]
     temperatures = temp
+
+    global d_currents
+    temp = dict(d_currents)
+    for i, name in enumerate(msg.name):
+        temp[name] = msg.direct_current[i]
+    d_currents = temp
+
+    global q_currents
+    temp = dict(q_currents)
+    for i, name in enumerate(msg.name):
+        temp[name] = msg.quadrature_current[i]
+    q_currents = temp
 
 #################################################################################################
 
@@ -34,12 +56,18 @@ def main():
             if len(temperatures) > len(motor_names):
                 motor_names = sorted(temperatures.keys())
                 values.append("time")
-                values.extend(motor_names)
+                values.extend([n + " temp" for n in motor_names])
+                values.extend([n + " d current" for n in motor_names])
+                values.extend([n + " q current" for n in motor_names])
             else:
-                values.append(rospy.get_time())
-                for name in motor_names:
-                    values.append(temperatures[name])
-            values = [str(x) for x in values]
+                values.append(str(rospy.get_time()))
+                def add_to_output(source):
+                    for name in motor_names:
+                        values.append(str(source[name]))
+                add_to_output(temperatures)
+                add_to_output(d_currents)
+                add_to_output(q_currents)
+
             print ','.join(values)
             sys.stdout.flush()
         r.sleep()
