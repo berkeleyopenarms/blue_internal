fbaee1456defe3e481c89f633aec66686c380f7e
Philipp <phil80301@berkeley.edu>
2018-11-08 23:07:35 -0800

    new ik node

---
+++ blue_teleop/scripts/blue_ik.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+import rospy
+import sys
+import actionlib
+from std_msgs.msg import Int32
+from std_msgs.msg import Float32
+from std_msgs.msg import Float64MultiArray
+from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import Pose
+from sensor_msgs.msg import JointState
+import PyKDL as kdl
+import kdl_parser_py.urdf as kdl_parser
+from trac_ik_python.trac_ik_wrap import TRAC_IK
+
+class BlueIK:
+    def _setup(self):
+        # load in ros parameters
+        self.baselink = rospy.get_param("blue_hardware/baselink")
+        self.endlink = rospy.get_param("blue_hardware/endlink")
+        urdf = rospy.get_param("/robot_description")
+        flag, self.tree = kdl_parser.treeFromString(urdf)
+
+        # build kinematic chain and fk and jacobian solvers
+        chain_ee = self.tree.getChain(self.baselink, self.endlink)
+        self.fk_ee = kdl.ChainFkSolverPos_recursive(chain_ee)
+        self.jac_ee = kdl.ChainJntToJacSolver(chain_ee)
+
+        # building robot joint state
+        self.num_joints = chain_ee.getNrOfJoints()
+
+        self.joint_names = rospy.get_param("blue_hardware/joint_names")
+        self.joint_names = self.joint_names[:self.num_joints]
+        if self.debug:
+          rospy.loginfo(self.joint_names)
+
+        self.joints = kdl.JntArray(self.num_joints)
+
+        # todo make seperate in abstract class
+        self.ik = TRAC_IK(self.baselink,
+                     self.endlink,
+                     urdf,
+                     0.005,
+                     1e-5,
+                    "Manipulation1")
+
+    def publish_ik_sol(self, target, joints):
+        # target is a target pose object
+        # joints is the starting joint seed
+        seed_state = []
+        for j in joints:
+            seed_state.append(j)
+
+        result = self.ik.CartToJnt(seed_state,
+                               target.position.x, target.position.y, target.position.z,
+                               target.orientation.x, target.orientation.y, target.orientation.z, target.orientation.w, )
+        msg = Float64MultiArray()
+        msg.data = result
+        if self.debug:
+          rospy.loginfo("ik result")
+          rospy.loginfo(result)
+        self.command_pub.publish(msg)
+
+    def update_joints(self, joint_msg):
+        if self.first:
+            return
+
+
+        temp_joints = kdl.JntArray(self.num_joints)
+        for i, n in enumerate(self.joint_names):
+            index = joint_msg.name.index(n)
+            temp_joints[i] = joint_msg.position[index]
+        self.joints = temp_joints
+
+        ## TODO make publishing the result seperately
+        self.publish_ik_sol(self.target_pose, self.joints)
+
+
+    def command_callback(self, msg):
+        temp_target = Pose()
+
+        temp_target.position.x = msg.pose.position.x # x
+        temp_target.position.y = msg.pose.position.y # y
+        temp_target.position.z = msg.pose.position.z # z
+
+        temp_target.orientation.x = msg.pose.orientation.x # qx
+        temp_target.orientation.y = msg.pose.orientation.y # qy
+        temp_target.orientation.z = msg.pose.orientation.z # qz
+        temp_target.orientation.w = msg.pose.orientation.w # qw
+
+        self.target_pose = temp_target
+
+        if self.first:
+            self.first = False
+
+    def __init__(self, debug=False):
+        rospy.init_node("blue_ik")
+        self.debug = debug
+        if self.debug:
+            self.debug_count = 0
+
+        self._setup()
+
+        self.target_pos = Pose()
+        self.first = True
+
+        rospy.Subscriber("pose_target/command", PoseStamped, self.command_callback)
+        self.command_pub = rospy.Publisher("blue_controllers/joint_position_controller/command", Float64MultiArray, queue_size=1)
+        rospy.Subscriber("/joint_states", JointState, self.update_joints)
+
+def main():
+    b = BlueIK(debug=True)
+    rospy.spin()
+
+if __name__ == "__main__":
+    main()
