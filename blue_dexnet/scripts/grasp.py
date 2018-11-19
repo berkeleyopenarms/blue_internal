d1948f41f5a0c1e533ea1ba82d49c07a0405c9ac
Philipp <phil80301@berkeley.edu>
2018-11-19 00:41:44 -0800

    draft of dex net node

--- blue_dexnet/scripts/grasp.py
+++ blue_dexnet/scripts/grasp.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import rospy
+import numpy as np
 import tf2_ros
 import tf2_geometry_msgs
 import sys
@@ -13,9 +14,54 @@ from sensor_msgs.msg import JointState
 import PyKDL as kdl
 import kdl_parser_py.urdf as kdl_parser
 from trac_ik_python.trac_ik_wrap import TRAC_IK
+from control_msgs.msg import (
+    GripperCommandAction,
+    GripperCommandGoal,
+)
+
+
+def cheb_points(n, k):
+    # return float(k)/float(n)
+    xk = np.cos( (2.0 * k  - 1.0) / (2.0 * n) * np.pi )
+    return (-xk + 1.0) / 2.0
+
+class GripperClient(object):
+    def __init__(self):
+        self._client = actionlib.SimpleActionClient(
+            "blue_controllers/gripper_controller/gripper_cmd",
+            GripperCommandAction,
+        )
+        # Wait 10 Seconds for the gripper action server to start or exit
+        if not self._client.wait_for_server(rospy.Duration(10.0)):
+            rospy.logerr("Exiting - Gripper Action Server Not Found")
+            rospy.signal_shutdown("Action Server not found")
+            sys.exit(1)
+
+    def command(self, position, effort):
+        goal = GripperCommandGoal()
+        goal.command.position = position
+        goal.command.max_effort = effort
+        self._client.send_goal(goal)
+
+    def stop(self):
+        self._client.cancel_goal()
+
+    def wait(self, timeout=0.1):
+        self._client.wait_for_result(timeout=rospy.Duration(timeout))
+        return self._client.get_result()
+
+    def call_grip(self, pos):
+        # commands a position 0 = open, 1 = close
+        position = -1.5 * pos
+        self.command(position, 2.5)
+        self.wait()
 
 class BlueIK:
     def _setup(self):
+        # set up chebychev points
+        self.resolution = 100
+        self.duration = 2
+
         # build tf listener
         self.tfBuffer = tf2_ros.Buffer()
         self.listener = tf2_ros.TransformListener(self.tfBuffer)
@@ -65,7 +111,17 @@ class BlueIK:
             return
         self.command_to_joint_state(result)
 
-    def command_to_joint_state(self, joints)
+    def command_to_joint_state(self, joints):
+        start_pos = np.array(self.joints).copy()
+        end_pos = np.array(joints).copy()
+        joint_diff = end_pos - start_pos
+        max_joint_diff = np.linalg.norm(joint_diff, np.inf)
+        for j in range(self.resolution + 1):
+            step = start_pos + (end_pos - start_pos) * cheb_points(self.resolution, j)
+            self._pub_js_msg(np.array(step))
+            rospy.sleep( np.abs(max_joint_diff) * duration * 1.0 / resolution)
+
+    def _pub_js_msg(self, joints):
         # joints is an array of 7 joint angles
         msg = Float64MultiArray()
         msg.data = joints
@@ -76,6 +132,7 @@ class BlueIK:
         self.command_pub.publish(msg)
         self.command_pub_ctc.publish(msg)
 
+
     def update_joints(self, joint_msg):
         if self.first:
             return
@@ -85,9 +142,6 @@ class BlueIK:
             index = joint_msg.name.index(n)
             temp_joints[i] = joint_msg.position[index]
         self.joints = temp_joints
-
-        ## TODO make publishing the result seperately
-        self.publish_ik_sol(self.target_pose, self.joints)
 
     def __init__(self, debug=False):
         self.debug = debug
@@ -107,6 +161,8 @@ def main():
     rospy.init_node("blue_ik")
     b = BlueIK(debug=True)
     home = [0, 0, 0, 0, 0, 0, 0]
+    drop_off = [0, 0, 0, 0, 0, 0, 0]
+    gc = GripperClient()
 
     # TODO init prime sense
     # TODO create grasp service client
@@ -116,7 +172,9 @@ def main():
         # TODO read in image from primesense
         # TODO make service call
         b.publish_ik_sol(from_service_call_PoseStamped message)
-
+        gc.call_grip(1.0):
+        b.command_to_joint_state(drop_off)
+        gc.call_grip(0.0):
 
 if __name__ == "__main__":
     main()
